9b050037262ca42e77177df84bcab21d69f39d2e
BT_1N3_12@BKTECH.LOCAL <BT_1N3_12@BKTECH.LOCAL>
2018-09-25 10:09:55 -0400

    Initial commit

---
+++ src/random.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class random {
+    public static void main (String[] args) {
+        double random = Math.random() * 5 + 1;
+        Scanner guess = new Scanner(System.in);
+        System.out.println("Give your best guess");
+        String compared = guess.nextLine();
+        Double newCompared = Double.parseDouble(compared);
+        if (newCompared == random) {
+            System.out.println("correct");
+        }
+        else {
+            System.out.println("actually its " + random);
+        }
+    }
+}
