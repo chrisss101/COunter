0954e39895da2c5228ed20daaf0e67c1e867a2e7
chrisss101 <Crafaniello9535@bths.edu>
2018-09-28 10:12:42 -0400

    rtt

--- src/computerguess.java
+++ src/computerguess.java
@@ -6,22 +6,25 @@ public class computerguess {
         System.out.println("Give me a number between 1 and 100");
         String num = range.nextLine();
         int Guess = (int)(Math.random() * 100 + 1);
-        int oldGuess = 0;
+        int max = 100;
+        int min = 0;
         System.out.println("I guess " + Guess);
-        String answer = range.nextLine();
+        String answer = "";
        // String compare = String.parseString(Guess);
-        if (answer.equals("correct")) {
-            System.out.println("I got it right");
-        }
-        else if (answer.equals("higher")) {
-            oldGuess = Guess;
-            Guess = (int)(Math.random() * (100 - oldGuess) + oldGuess + 1);
-            System.out.println(Guess);
-        }
-        else if (answer.equals("lower")) {
-            oldGuess = Guess;
-            Guess = (int)(Math.random() * oldGuess + 1);
-            System.out.println(Guess);
-        }
+          while (answer != "correct") {
+              answer = range.nextLine();
+              if (answer.equals("correct")) {
+                  System.out.println("I got it right");
+              } else if (answer.equals("higher")) {
+                 min = Guess;
+                  Guess = (int) (Math.random() * (max - min) + min + 1);
+                  System.out.println(Guess);
+              } else if (answer.equals("lower")) {
+                  max = Guess;
+                  Guess = (int) (Math.random() * (max - min) + 1);
+                  System.out.println(Guess);
+              }
+          }
     }
+
 }
